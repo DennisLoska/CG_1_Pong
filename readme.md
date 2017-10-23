192277916d70df7d61f93e9b65b54f5fa1525444
DennisLoska <linksy@hotmail.de>
2017-10-23 13:22:35 +0200

    adding documentation for intellij integration - project works!

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Pong
+
+## IntelliJ Integration
+
+- Set VM Options in the run configuration to **-Djava.library.path=Libs/**
+- create a folder _Libs_ in the root directory (is there from the start)
+- put the respective files for your OS in the folder Libs
--- src/de/berlin/htw/Ex1PONG.java
+++ src/de/berlin/htw/Ex1PONG.java
@@ -1,8 +1,33 @@
 package de.berlin.htw;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.Display;
+
 public class Ex1PONG {
 
+    private static int width = 800;
+    private static int height = 600;
+
     public static void main(String[] args) {
-        System.out.println("Hello CG!");
+        try {
+            Display.setDisplayMode(new DisplayMode(width, height));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        GL11.glPointSize(5);
+        GL11.glBegin(GL11.GL_POINTS);
+        GL11.glColor3f(1, 0, 1);
+
+        GL11.glVertex2f(0, 0);
+        GL11.glVertex2f(3, 4.5f);
+        GL11.glEnd();
+
+        while (!Display.isCloseRequested()) Display.update();
+
     }
 }
